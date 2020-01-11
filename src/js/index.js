b8f011d5c099e08878c84ea7d88716a264b129a3
jesusoterove <jesusoterove@gmail.com>
2020-01-11 15:57:05 -0500

    Added rollup as javascript transpiler

---
+++ src/js/index.js
@@ -0,0 +1,62 @@
+import properties from './properties';
+import '../css/style.css';
+
+class App {
+
+    constructor() {
+        this.hooks = {};
+        this.plugins = {};
+    }
+
+    registerHook(id, callback, priority) {
+        if (!this.hooks[id]) this.hooks[id] = [];
+        //ToDo: Register the hook according to the priority
+        this.hooks[id].push({ callback, priority });
+    }
+
+    init(options) {
+        //Parse options
+        this.parseOptions(options);
+
+        //Load active plugins
+        for(let plugin of properties.plugins) {
+            if (!plugin.active) continue;
+            var objectName = plugin.id.split('.').map(p => p[0].toUpperCase() + p.substr(1)).join('');
+            if (objectName in tepuyEditor) {
+                this.plugins[plugin.id] = new tepuyEditor[objectName];
+            }
+            else {
+                import (`./js/plugins/${plugin.id}/component.js`).then(function (pClass) {
+                    this.plugins[plugin.id] = new pClass();
+                })
+            }
+        }
+
+        this.invokeHook('gui_initialize');
+    }
+
+    parseOptions(options) {
+        var options = Object.assign({
+            container: "#tepuy-editor"
+        }, options);
+
+        if (typeof(options.container) === 'string') {
+            this.container = document.querySelector(options.container);
+        }
+        else {
+            this.container = options.container;
+        }
+    }
+
+    invokeHook(id, params = null) {
+        if (!this.hooks[id]) return;
+
+        var hooks = this.hooks[id];
+        for(let hook of hooks) {
+            hook.callback.apply(null, params);
+        }
+    }
+}
+
+const app = new App();
+export { app as App};
