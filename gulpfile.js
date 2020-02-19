bab30bdf63df17f36e816c054ab2e05c58faf5b2
jesusoterove <jesusoterove@gmail.com>
2020-02-19 15:05:49 -0500

    Added jquery-ui structure css

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,9 @@ const vendorjs = [
 ];
 
 const vendorcss = [
-    'node_modules/jquery-ui-dist/jquery-ui.theme.min.css'
+    'node_modules/jquery-ui-dist/jquery-ui.min.css',
+    //'node_modules/jquery-ui-dist/jquery-ui.theme.min.css',
+    './vendor/css/*.css'
 ];
 
 function joinVendorCss() {
