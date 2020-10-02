f5a97cc6b567e18251988dea14370eb0db269b61
cirano <davidherney@gmail.com>
2020-10-02 01:20:03 -0500

    New moodle integration plugins

---
+++ src/plugins/auth.moodle/plugin.js
@@ -0,0 +1,19 @@
+export class AuthMoodle {
+    authenticate() {
+
+        return new Promise((resolve, reject) => {
+
+//            var TepuyAPI = { currentuser: {id: 1, username: 'cirano', fullname: 'David', imageurl: ''} }
+            if (typeof(TepuyAPI) != 'undefined') {
+                resolve({
+                    id: TepuyAPI.currentuser.id,
+                    userName: TepuyAPI.currentuser.username,
+                    name: TepuyAPI.currentuser.fullname,
+                    profile_image: TepuyAPI.currentuser.imageurl
+                });
+            } else {
+                reject();
+            }
+        });
+    }
+}
